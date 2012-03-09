edb8919dd9b4745de6d45151d34ddfda580cf3af
Matthieu Riviere <poldenton@leukensis.org>
2012-03-09 17:03:15 +0100

    Added delete_last_post helper to admin script

--- viewer/admin.rb
+++ viewer/admin.rb
@@ -9,4 +9,11 @@ def run_req(sql)
 	return @db.exec(sql).values
 end
 
+def delete_last_post(website_id)
+	r = @db.exec("SELECT uid FROM posts WHERE website_id='#{website_id}' ORDER BY uid DESC LIMIT 1")
+	id = r[0]['uid']
+	run_req("DELETE FROM readeds WHERE post_id='#{id}'")
+	run_req("DELETE FROM posts WHERE uid='#{id}'")
+end
+
 IRB.start(__FILE__)
